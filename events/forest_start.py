95ac564605fa5a68290fdf9fc5e1270e76ff09ba
Laurens <laurenssouthgate@gmail.com>
2025-06-06 23:36:38 +0100

    outlining more choices in the forest start event

--- events/forest_start.py
+++ events/forest_start.py
@@ -35,6 +35,7 @@ class ForestStart(Event) :
         player.decrease_stat("humility", 7)
         player.increase_stat("wrath", 10)
         player.increase_stat("pride", 7)
+
         print("\nYou give in to the sense of anger at the situation, and begin in earnest to search the surroundings for any sign that might lead to you the perpetrators")
         print("\nYou notice two sets of footprints going off along the path")
 
@@ -50,7 +51,29 @@ class ForestStart(Event) :
 
         def give_up(player) :
             pass 
+    
+    def pray(self, player) :
+        player.increase_stat("dilligence", 3)
+        player.increase_stat("humility", 5)
+        player.decrease_stat("pride", 5)
 
+        print("\nYou make the sign of the cross and bow your head. 'God forgive them, and forgive me a sinner' you say. You make your way back to the path.")
+
+        choices = [
+            Choice("Go right", go_right(player)),
+            Choice("Go left", go_left(player))
+        ]
+
+        present_choices(choices, player)
+
+        def go_right(player) :
+            pass
+
+        def go_left(player) :
+            pass
+
+    def call_for_help(self, player) :
+        pass
 
 
 
